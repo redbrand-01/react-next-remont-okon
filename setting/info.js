f0bdb1d1e89fecfc0478682221be6479db4f942e
redbrand-01 <84344413+redbrand-01@users.noreply.github.com>
2021-10-03 20:03:29 +0500

    Update info.js

--- setting/info.js
+++ setting/info.js
@@ -2,7 +2,7 @@ export const config = {
     phone: '+7 (342) 204-02-34',
     call: '+73422040234',
     phonetext: 'Вызов мастера',
-    email: 'teplyidom.perm@gmail.com',
+    email: 'teplyidom111.perm@gmail.com',
     work: 'ПН - СБ 9:00 - 20:00',
     textwork: 'Время работы'
 }
